798ac2600be4333a79eead804fc35ae9e0fcfc2a
burnettemk <wafflesxyz@gmail.com>
2023-09-05 08:10:31 -0700

    Parameterized Queries

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -8,11 +8,17 @@ interface Post {
     userId: number;
   }
 
-const usePosts = () => useQuery<Post[], Error>({
-    queryKey: ['posts'],
+const usePosts = (userId: number | undefined) => useQuery<Post[], Error>({
+    // if we wanted to build our own API, the data should go from more general to more specific
+    // /users/1/posts
+    queryKey: userId ? ['users', userId, 'posts'] : ['posts'],
     queryFn: () => 
         axios
-        .get('https://jsonplaceholder.typicode.com/posts')
+        .get('https://jsonplaceholder.typicode.com/posts', {
+            params: {
+                userId,
+            }
+        })
         .then(res => res.data),
     staleTime: 1 * 60 * 1000, // 1m
 })
