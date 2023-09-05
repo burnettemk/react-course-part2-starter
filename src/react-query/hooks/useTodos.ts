cfbc2c3f2d91034c8e890e46d9c6e55aabbbcfb6
burnettemk <wafflesxyz@gmail.com>
2023-09-04 17:28:38 -0700

    Customize query settings

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -17,6 +17,12 @@ const useTodos = () => {
     return useQuery<Todo[], Error>({
         queryKey: ["todos"],
         queryFn: fetchTodos,
+        retry: 3,
+        cacheTime: 300_000, //5m The amount of time before inactive queries get removed from the cache
+        staleTime: 10 * 1000, //10s
+        // refetchOnWindowFocus: false,
+        // refetchOnReconnect: false,
+        // refetchOnMount: false,
     });
 }
 
