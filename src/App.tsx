d53f58ec2ea4482e6a29c6b20df1a69a85118cff
burnettemk <wafflesxyz@gmail.com>
2023-09-18 22:09:48 -0700

    Create a custom login provider

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,19 @@ import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
 import loginReducer from "./state-management/reducers/loginReducer";
 import LoginContext from "./state-management/contexts/loginContext";
+import LoginProvider from "./state-management/LoginProvider";
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-  const [user, loginDispatch] = useReducer(loginReducer, "");
 
   return (
     <>
-      <LoginContext.Provider value={{ user, dispatch: loginDispatch }}>
+      <LoginProvider>
         <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
           <NavBar />
           <HomePage />
         </TasksContext.Provider>
-      </LoginContext.Provider>
+      </LoginProvider>
     </>
   );
 }
