5b5defdb3b539730cc8c89b88803204a5188b434
burnettemk <wafflesxyz@gmail.com>
2023-10-24 11:17:48 -0700

    Passing data w/ params & getting data about routes

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,13 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  // console.log(searchParams.get("name")); //has an effect that causes component to be impure
+  const location = useLocation();
+
+  console.log(location);
+
   return <p>User</p>;
 };
 
