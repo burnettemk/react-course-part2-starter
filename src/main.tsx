c00c54ae566b51ced3ee1e2ce8f90dc8de6b5fc4
burnettemk <wafflesxyz@gmail.com>
2023-10-24 10:56:10 -0700

    Change from app to react router

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import router from "./routing/routes";
+import { RouterProvider } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
